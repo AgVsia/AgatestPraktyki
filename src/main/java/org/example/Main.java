62c0312b5854859666a146266bf2712797fcf2a2
Agnieszka Antonczak <agnieszka.w.antonczak@gmail.com>
2023-02-09 18:03:25 +0100

    Added TODO tasks

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,11 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Zadanie1 Wyświetl na wyjściu napis "Hello World"
+        System.out.println("Hello World");
+
+        //todo Zadanie2 Wyswietl swoje imie i nazwisko w osobnych wierszach
+
+        //todo Zadanie3 Wyswietl swoja date urodzenia
     }
 }
