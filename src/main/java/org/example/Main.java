0a73bd1d2f80d667853a9dfa3a2ac1355c6cac7c
AgVsia <agnieszkazet9@wp.pl>
2023-02-11 19:36:23 +0100

    tasks of String method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,40 +2,37 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        //Zadanie1 Wyświetl na wyjściu napis "Hello World"
-        System.out.println("Hello World");
-
-        //Zadanie2 Wyswietl swoje imie i nazwisko w osobnych wierszach
-        System.out.println("Agnieszka");
-        System.out.println("Gatak");
-
-        //Zadanie3 Wyswietl swoja date urodzenia
-        System.out.println("14.10.1988");
-
-        int var; //deklaracja zmienej
-        var = 10; //inicjalizacja zmiennej
-
-        float varFloat = 3.99F;
-        double varDouble = 3.99;
-        System.out.println(3.99);
-
-        char varChar = 'A';
-
-        boolean varBoolean = false;
-
-        System.out.println(varFloat);
-
-        final int VAR_INT = 10;
-
-        System.out.println(10 + 3);
-        System.out.println(10 > 9);
-
-        String varString1 = "Hej!";
-        String varString2 = "Adam";
-        System.out.println(varString1 + " " + varString2);
-        System.out.println(varString1.charAt(0));
-        char firstLetter = varString1.charAt(0);
+        //zadanie 1 Wyświetl pierwszą literę wyrażenia "Ala ma kota."
+        String phrase = "Ala ma kota";
+        char firstLetter = phrase.charAt(0);
         System.out.println(firstLetter);
-
+        //zadanie 2 Wyświetl ostatni znak wyrażenia "Ala ma kota."
+        char lastLetter = phrase.charAt(10);
+        System.out.println(lastLetter);
+        //Wyświetl długość wyrażenia "Ala ma kota."
+        int lengthOfPhrase = phrase.length();
+        System.out.println("length of phrase" + " " + lengthOfPhrase);
+        // Wyświetl wyrażenie "Ala ma kota." wielkimi literami
+        String phraseOfUpperCase = phrase.toUpperCase();
+        System.out.println(phraseOfUpperCase);
+        //Wyświetl wyrażenie "Ala ma kota." małymi literami
+        String phraseOfLowerCase = phrase.toLowerCase();
+        System.out.println(phraseOfLowerCase);
+        //Wyświetl pod jakim indeksem znajduje się litera "m" w wyrażeniu "Ala ma kota."
+        int indexOfSpecificLetter = phrase.indexOf("m");
+        System.out.println("index of letter m " + indexOfSpecificLetter);
+        //Zamień wyrażenie "Ala ma kota." na "Ala ma koty."
+        String replacedPhrase = phrase.replace("kota","koty");
+        System.out.println(replacedPhrase);
+        //Zamień wszystkie litery "a" na "b" w wyrażeniu "Ala ma kota."
+        String replacedPhraseFromAToB = phrase.replace("a","b");
+        System.out.println(replacedPhraseFromAToB);
+        //Z wyrażenia "Ala ma kota." wyłuskaj wyraz "ma"
+        String substringOfprase = phrase.substring(4,6);
+        System.out.println(substringOfprase);
+        //Do wyrażenia "Ala ma kota." dodaj wyrażenie "A kot ma Alę" tak by wyświetliły się w jedenj linii.
+        String concatenatePhrase = phrase.concat(", A kot ma Alę");
+        System.out.println(concatenatePhrase);
+        System.out.print(concatenatePhrase);
     }
 }
