65a2e144ee5c608ec3125d602114541417a47538
Agnieszka Antonczak <agnieszka.w.antonczak@gmail.com>
2023-02-17 00:26:10 +0100

    Added more tasks

--- src/main/java/org/example/InOutTasks.java
+++ src/main/java/org/example/InOutTasks.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 public class InOutTasks {
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
-        System.out.print("Program prosi o wartość: ");
+        System.out.print("Wpisz string: ");
         String stringValueFromUser = in.nextLine();
         System.out.println("User wpisał: " + stringValueFromUser);
 
-        System.out.print("Program prosi o wartość: ");
+        System.out.print("Wpisz int: ");
         int intValueFromUser = in.nextInt();
-        System.out.println("User wpisał taki int: " + intValueFromUser);
+        System.out.println("User wpisał: " + intValueFromUser);
     }
 }
