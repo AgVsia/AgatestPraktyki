1c962253a1aeb739a209ddeee463740bb84d49ac
Agnieszka Antonczak <agnieszka.w.antonczak@gmail.com>
2023-03-16 18:33:21 +0100

    Theory about loops

--- src/main/java/org/example/Loops/Loops.java
+++ src/main/java/org/example/Loops/Loops.java
@@ -4,5 +4,27 @@ public class Loops {
     public static void main(String[] args){
 
     //Do czego służy pętla, rodzaje pętli, zasięg zmiennych
+        //pętla for
+        //inicjacja licznika ; warunek ; sposob zmiany licznika
+        for(int i = 0; i < 5; i++){
+            System.out.println("Witaj świecie z pęli for. Licznik: " + i);
+        }
+
+        //pętla while
+        // wykonuje blok tak długo dopóki warunek jest true
+        int j = 0;
+        while(j < 5){
+            System.out.println("Witaj świecie z pętli while. Licznik: " + j);
+            j++;
+            //j = j + 1;
+            //j += 1;
+        }
+
+        //pętla do .. while - zawsze wykona się co najmniej raz
+        int k = 0;
+        do {
+            System.out.println("Witaj świecie z pętli do while. Licznik: " + k);
+            k++;
+        } while (k < 0);
     }
 }
