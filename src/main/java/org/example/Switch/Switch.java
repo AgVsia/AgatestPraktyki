b67e9efbd7b538b422ca2837fce37430618d5ba5
Agnieszka Antonczak <agnieszka.w.antonczak@gmail.com>
2023-03-30 19:56:17 +0200

    Added example of using switch

--- src/main/java/org/example/Switch/Switch.java
+++ src/main/java/org/example/Switch/Switch.java
@@ -1,8 +1,35 @@
 package org.example.Switch;
 
+import java.util.Scanner;
+
 public class Switch {
     public static void main(String[] args){
 
-    //Do czego switch, instrukcje przerywajace przeplyw sterowania
+        //wykonywanie programu zacznie się od etykiety case, która pasuje do wybranej opcji
+        // i jest kontunowane aż do napotkania instrukcji break
+        Scanner in = new Scanner(System.in);
+        System.out.println("Wybierz opcję (1, 2, 3, 4)");
+        int choice = in.nextInt();
+
+        switch (choice){
+            case 1, 0:
+                System.out.println("Wybrano opcję 1 lub 0");
+                System.out.println("bla bla");
+                System.out.println("dalsze instrukcje ...");
+                break;
+            case 2:
+                System.out.println("Wybrano opcję 2");
+                break;
+            case 3:
+                System.out.println("Wyborano opcję 3");
+                break;
+            case 4:
+                System.out.println("Wybrano opcję 4");
+                break;
+            default:
+                System.out.println("Niepoprawna wartość");
+                break;
+        }
+
     }
 }
